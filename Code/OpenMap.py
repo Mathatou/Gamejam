1b77df732f650f83a52095ef7b1236f32e6baa70
Mathatou <130315027+Mathatou@users.noreply.github.com>
2025-09-16 15:51:26 +0200

    Fin des maps

--- Code/OpenMap.py
+++ Code/OpenMap.py
@@ -8,8 +8,9 @@ SCREEN_TITLE = "Plateforme 2D avec map fixe"
 TILE_SCALING = 1.48
 
 # Les différentes maps de notre jeu 
-MAP_FILE = ".\..\Tileset\oak_woods_v1.0\First_Map.tmx" # N°1
-#MAP_FILE = ".\..\Tileset\Dungeon Tile Set\Last_Map.tmx" # N°3
+#MAP_FILE = ".\..\Tileset\Maps\First_Map.tmx" # N°1
+#MAP_FILE = ".\..\Tileset\Maps\Second_Map.tmx" # N°2
+MAP_FILE = ".\..\Tileset\Maps\Last_Map.tmx" # N°3
 
 
 class MyGame(arcade.Window):
