1a82033d617878481677832b1de0e94f90787f38
Mathatou <130315027+Mathatou@users.noreply.github.com>
2025-09-16 11:33:27 +0200

    Ajout des tileset

---
+++ Tileset/oak_woods_v1.0/Backgrounds.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Backgrounds" tilewidth="320" tileheight="180" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="oak_woods_v1.0/background/background_layer_2.png" width="320" height="180"/>
+ </tile>
+ <tile id="1">
+  <image source="oak_woods_v1.0/background/background_layer_3.png" width="320" height="180"/>
+ </tile>
+ <tile id="2">
+  <image source="oak_woods_v1.0/background/background_layer_1.png" width="320" height="180"/>
+ </tile>
+</tileset>
